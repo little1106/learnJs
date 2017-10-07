38925b761d8ec16831a23e14462edb4b32f16378
penghuiwu <1318771947@qq.com>
2017-10-07 21:48:23 +0800

    css Style

--- TypeScript/angular/angular-tour-of-heroes/src/app/heroes.component.ts
+++ TypeScript/angular/angular-tour-of-heroes/src/app/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -15,7 +16,7 @@ export class HeroesComponent implements OnInit{
   selectedHero: Hero;
   heroes: Hero[];
 
-  constructor(private heroService: HeroService){ };
+  constructor(private heroService: HeroService, private router: Router){ };
 
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -28,4 +29,8 @@ export class HeroesComponent implements OnInit{
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   };
+
+  gotoDetail(): void{
+    this.router.navigate(['./detail',this.selectedHero.id]);
+  }
 }
