2c91a2cb8ed475b5ad8e43b2285b993e8b91e045
penghuiwu <1318771947@qq.com>
2017-10-25 09:00:59 +0800

    seachComponent

--- TypeScript/angular/angular-tour-of-heroes/src/app/app.module.ts
+++ TypeScript/angular/angular-tour-of-heroes/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
+import { HeroSearchComponent } from './hero-search.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -22,7 +23,8 @@ import { InMemoryDataService }  from './in-memory-data.service';
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
