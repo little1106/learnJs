4db183167603d90a3d4b446490f5e8efb1819381
penghuiwu <1318771947@qq.com>
2017-12-09 20:45:20 +0800

    prop and emit(父子组件数据传递)

--- vue/vue-todolist/app.js
+++ vue/vue-todolist/app.js
@@ -1,5 +1,44 @@
+
+var sonEmit = {
+    template: `<p><span>sonEmit: </span><input v-model='messageSon' v-on:input='emitMessage()'></p>`,
+    data: function(){
+            return {
+                messageSon: 'default'
+            }
+    },
+    methods: {
+        emitMessage: function(){
+            this.$emit('change',this.messageSon)
+        }
+    }
+}
+
+var sonProp = {
+    props: ['messageSon'],
+    template: `<p><span>sonProp: </span><span>{{messageSon}}</span></p>`
+}
+
 var Child = {
-    template: '<div>A custom component!</div>'
+    template: `<div>A custom component!
+                    <p><span>Father: </span>{{message}}</p>
+                    <sonemit-component v-on:change = "changeMessage($event)"></sonemit-component>
+                    <sonprop-component :messageSon = "message"></sonprop-component>
+
+               </div>`,
+    data: function(){
+            return {
+                    message: 'Default'
+                   }
+               },
+               methods:{
+                    changeMessage: function($event){
+                        this.message = $event;
+                    }
+               },
+    components: {
+        'sonemit-component': sonEmit,
+        'sonprop-component': sonProp
+    }
 }
 
 new Vue({
